7b9e2b6c0f2668f8b459a7b68a6948ff43a596c8
tangkaiyang <945541696@qq.com>
2019-04-29 17:30:35 +0800

    33

---
+++ 第一章绪论/sqrt.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2019/4/29 10:05
+# @Author   : tangky
+# @Site     : 
+# @File     : sqrt.py
+# @Software : PyCharm
+
+def sqrt(x):
+    y = 1.0
+    while abs(y * y - x ) > 1e-6:
+        y = (y + x/y)/2
+    return y
+
+print(sqrt(2))
